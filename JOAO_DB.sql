72e5189d5b127a0cde2c5a846ffc4dce23c3d992
twiggier <70548639+twiggier@users.noreply.github.com>
2023-02-10 10:52:37 -0300

    Testando os conhecimentos

--- JOAO_DB.sql
+++ JOAO_DB.sql
@@ -1,14 +1,14 @@
 INSERT INTO cidades (id_cidade, uf_cidade, nome_cidade) VALUES
-    (1, 'SP', 'São Paulo');
+    (1, 'SP', 'Sï¿½o Paulo');
 INSERT INTO cidades (id_cidade, uf_cidade, nome_cidade) VALUES
     (2, 'RJ', 'Rio de Janeiro'); 
 INSERT INTO cidades (id_cidade, uf_cidade, nome_cidade) VALUES
     (3, 'MG', 'Minas Gerais');
-INSERT INTO clientes (id_cliente, endereço_cliente, nome_cliente, numero_cliente, bairro_cliente) VALUES
+INSERT INTO clientes (id_cliente, endereï¿½o_cliente, nome_cliente, numero_cliente, bairro_cliente) VALUES
     (1, 'Rua Carlos vieira 130', 'Juan', 998765432, 'Vila Unida');
-INSERT INTO clientes (id_cliente, endereço_cliente, nome_cliente, numero_cliente, bairro_cliente) VALUES
+INSERT INTO clientes (id_cliente, endereï¿½o_cliente, nome_cliente, numero_cliente, bairro_cliente) VALUES
     (2, 'Rua Esmeralda 185', 'Leandro', 997869433, 'Bandeirantes');
-INSERT INTO clientes (id_cliente, endereço_cliente, nome_cliente, numero_cliente, bairro_cliente) VALUES
+INSERT INTO clientes (id_cliente, endereï¿½o_cliente, nome_cliente, numero_cliente, bairro_cliente) VALUES
     (3, 'Rua Costa Dias 271', 'Emerson', 993621580, 'Manteigas');
 INSERT INTO estados (id_estado, nome_estado, sigla_estado) VALUES
     (1, 'Distrito Federal', 'DF');
